d9fa15edb36867d4254c2e45c480db21b248d14b
Ratnesh <ratneshjain40@gmail.com>
2022-08-24 21:10:49 +0530

    WebAuth Flow Completed v0.1

--- controllers/webauthn.js
+++ controllers/webauthn.js
@@ -1,40 +1,47 @@
 const {
     generateRegistrationOptions,
     verifyRegistrationResponse,
-} = require('@simplewebauthn/server');
+    generateAuthenticationOptions,
+    verifyAuthenticationResponse,
+} = require("@simplewebauthn/server");
 
-const { User } = require('../models/user');
-const { randomToken } = require('../utils/passwords');
+const base64url = require("base64url");
 
-const rpName = 'SimpleWebAuthn Example';
-const rpID = 'localhost';
-const origin = `https://${rpID}`;
+const ErrorResponse = require("../utils/errorResponse");
+
+const { User, Authenticator } = require("../models/user");
+const { randomToken } = require("../utils/passwords");
+
+const rpName = "SimpleWebAuthn Example";
+const rpID = "localhost";
+const origin = `http://${rpID}:3000`;
 
 exports.registerRequest = async (req, res, next) => {
     try {
+        // Telling the browser what type of authenticator we want to use and sending the challenge
         const user = req.session.user;
-        const userAuthenticators = user.authenticators;
+        const userAuthenticators = await Authenticator.find({ user: user._id });
 
         const options = generateRegistrationOptions({
             rpName,
             rpID,
-            userID: user.id,
+            userID: user._id,
             userName: user.username,
             // Prevent users from re-registering existing authenticators
-            excludeCredentials: userAuthenticators.map(authenticator => ({
+            excludeCredentials: userAuthenticators.map((authenticator) => ({
                 id: authenticator.credentialID,
-                type: 'public-key',
+                type: "public-key",
             })),
             authenticatorSelection: {
-                authenticatorAttachment: 'platform',
-                userVerification: 'required',
+                authenticatorAttachment: "platform",
+                userVerification: "required",
             },
         });
 
         options.pubKeyCredParams = [];
         const params = [-7, -257];
         for (let param of params) {
-            options.pubKeyCredParams.push({ type: 'public-key', alg: param });
+            options.pubKeyCredParams.push({ type: "public-key", alg: param });
         }
 
         req.session.userChallenge = options.challenge;
@@ -43,13 +50,14 @@ exports.registerRequest = async (req, res, next) => {
             success: true,
             options,
         });
-
     } catch (error) {
         next(error);
     }
-}
+};
 
 exports.registerResponse = async (req, res, next) => {
+    // verifing the challenge and origin from the browser,
+    // if they match, then we can register the authenticator with the given credential
     try {
         const { body } = req;
         const expectedChallenge = req.session.userChallenge;
@@ -62,46 +70,52 @@ exports.registerResponse = async (req, res, next) => {
 
         if (verification.verified) {
             const { registrationInfo } = verification;
-            const { credentialPublicKey, credentialID, counter } = registrationInfo;
+            const { credentialPublicKey, credentialID, counter } =
+                registrationInfo;
 
-            const newAuthenticator = {
-                credentialID,
-                credentialPublicKey,
+            const authenticator = await Authenticator.create({
+                user: req.session.user._id,
+                credentialID: base64url.encode(credentialID),
+                credentialPublicKey: base64url.encode(credentialPublicKey),
                 counter,
-            };
+                transports: ["internal"],
+            });
 
-            const user = await User.findByIdAndUpdate(req.session.user.id, { $push: { authenticators: newAuthenticator } });
-            if (!user) {
-                throw new ErrorResponse('Could not add user', 404);
+            if (!authenticator) {
+                throw new ErrorResponse("Could not add authenticators", 404);
             }
 
             delete req.session.challenge;
 
             res.status(200).json({
                 success: true,
-                message: 'Registration successful',
+                message: "Registration successful",
             });
         } else {
-            throw new ErrorResponse('Registration failed', 500);
+            throw new ErrorResponse("Registration failed", 500);
         }
-
     } catch (error) {
         next(error);
     }
-}
+};
 
 exports.signInRequest = async (req, res, next) => {
+    // Giving the browser the challenge and the options to sign in with authenticator previously registered
     try {
-        const user = req.session.user;
-        const userAuthenticators = user.authenticators;
+        const user = await User.findById(req.session.user._id);
+        // console.log(user);
+        const userAuthenticators = await Authenticator.find({ user: user._id });
 
         const options = generateAuthenticationOptions({
             // Require users to use a previously-registered authenticator
-            allowCredentials: userAuthenticators.map(authenticator => ({
-                id: authenticator.credentialID,
-                type: 'public-key',
+            rpName,
+            rpID,
+            allowCredentials: userAuthenticators.map((authenticator) => ({
+                id: base64url.toBuffer(authenticator.credentialID),
+                type: "public-key",
+                transports: authenticator.transports,
             })),
-            userVerification: 'required',
+            userVerification: "required",
         });
 
         req.session.userChallenge = options.challenge;
@@ -110,59 +124,90 @@ exports.signInRequest = async (req, res, next) => {
             success: true,
             options,
         });
-
     } catch (error) {
         next(error);
     }
-}
+};
 
 exports.signInResponse = async (req, res, next) => {
+    // cross checking the produced credentials with the ones in the database
     try {
         const { body } = req;
-        const user = req.session.user;
         const expectedChallenge = req.session.userChallenge;
-        // can check authenticator against user.authenticators to prevent extra db query
-        const authenticator = await User.find({ 'authenticators.credentialID': body.id });
+        const user = await User.findById(req.session.user._id);
+
+        let authenticator = await Authenticator.findOne({
+            user: user._id,
+            credentialID: body.id,
+        });
 
         if (!authenticator) {
-            throw new ErrorResponse(`Could not find authenticator ${body.id} for user ${user.id}`, 500);
+            throw new ErrorResponse("Authenticator not found", 404);
+        }
+        const authenticatorObject = {
+            credentialID: base64url.toBuffer(authenticator.credentialID),
+            credentialPublicKey: base64url.toBuffer(
+                authenticator.credentialPublicKey
+            ),
+            counter: authenticator.counter,
+        };
+
+        if (!authenticator) {
+            throw new ErrorResponse(
+                `Could not find authenticator ${body.id} for user ${user._id}`,
+                500
+            );
         }
 
-        let verification = await verifyAuthenticationResponse({
+        let verification = verifyAuthenticationResponse({
             credential: body,
             expectedChallenge,
             expectedOrigin: origin,
             expectedRPID: rpID,
-            authenticator,
+            authenticator: authenticatorObject,
         });
+
+        const { authenticationInfo } = verification;
+        const { newCounter } = authenticationInfo;
+
+        const updateCounter = await Authenticator.findOneAndUpdate(
+            { credentialID: body.id },
+            { counter: newCounter }
+        );
+
+        if (!updateCounter) {
+            throw new ErrorResponse(
+                `Could not update counter for user ${user.id}`,
+                500
+            );
+        }
 
         if (verification.verified) {
             res.status(200).json({
                 success: true,
-                options,
             });
-            const { authenticationInfo } = verification;
-            const { newCounter } = authenticationInfo;
-            const counter = await User.findByIdAndUpdate(user.id, { 'authenticators.$.counter': newCounter });
-            if (!counter) {
-                throw new ErrorResponse(`Could not update counter for user ${user.id}`, 500);
-            }
         } else {
-            res.status(500).json({ success: false, message: 'Authentication failed' });
+            res.status(500).json({
+                success: false,
+                message: "Authentication failed",
+            });
         }
-
     } catch (error) {
         next(error);
     }
-}
+};
 
 exports.getRemoteToken = async (req, res, next) => {
     try {
         const token = randomToken(32);
-        req.session.remoteToken = { token: token, time: Date.now(), used: false };
+        req.session.remoteToken = {
+            token: token,
+            time: Date.now(),
+            used: false,
+        };
         const valid = await req.session.save();
         if (!valid) {
-            throw new ErrorResponse('Could not save session', 500);
+            throw new ErrorResponse("Could not save session", 500);
         }
         res.status(200).json({
             success: true,
@@ -171,4 +216,4 @@ exports.getRemoteToken = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-}
+};
--- models/User.js
+++ models/User.js
@@ -1,26 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		required: [true, 'Please enter username'],
-		unique: [true, 'Username already exists'],
-	},
-	hash: {
-		type: String,
-		required: true,
-	},
-	salt: {
-		type: String,
-		required: true,
-	},
-	authenticators: [{
-		credentialID: String,
-		credentialPublicKey: String,
-		counter: Number,
-	}],
+    username: {
+        type: String,
+        required: [true, "Please enter username"],
+        unique: [true, "Username already exists"],
+    },
+    hash: {
+        type: String,
+        required: true,
+    },
+    salt: {
+        type: String,
+        required: true,
+    },
+    authenticators: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Authenticator",
+        },
+    ],
 });
 
-const User = mongoose.model('User', userSchema);
+const authenticatorSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    credentialID: String,
+    credentialPublicKey: String,
+    counter: Number,
+    transports: [String],
+});
 
-module.exports = { User };
+const User = mongoose.model("User", userSchema);
+const Authenticator = mongoose.model("Authenticator", authenticatorSchema);
+
+module.exports = { User, Authenticator };
