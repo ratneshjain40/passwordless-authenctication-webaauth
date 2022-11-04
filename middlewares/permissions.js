ea4d789581da7e3a244d8b6fb47fe7fcf4fc83d6
Ratnesh <ratneshjain40@gmail.com>
2022-11-04 18:30:37 +0530

    Permission Fixes

--- middlewares/permissions.js
+++ middlewares/permissions.js
@@ -1,27 +1,35 @@
-import ErrorResponse from '../utils/errorResponse.js';
+import ErrorResponse from "../utils/errorResponse.js";
 
 export const PERMISSIONS = {
-	ADMIN: 0,
-	USER: 1,
-	GUEST: 2,
+    ADMIN: 0,
+    USER: 1,
+    GUEST: 2,
 };
 
 export const checkPermissionLevel = (type) => {
-	if (PERMISSIONS[type] === undefined) {
-		throw new ErrorResponse('Invalid permission level', 401);
-	}
-	return (req, res, next) => {
-		try {
-			if (req.session.user.permissions <= PERMISSIONS[type]) {
-				next();
-			} else {
-				throw new ErrorResponse(
-					'You are not authorized to view this resource',
-					401
-				);
-			}
-		} catch (error) {
-			next(error);
-		}
-	};
+    if (PERMISSIONS[type] === undefined) {
+        throw new ErrorResponse("Invalid permission level", 401);
+    }
+    return (req, res, next) => {
+        try {
+
+            if (req.session.user.permissions === undefined) {
+                throw new ErrorResponse(
+                    "You are do not have the permissions to view this resource",
+                    401
+                );
+            }
+			
+            if (req.session.user.permissions <= PERMISSIONS[type]) {
+                next();
+            } else {
+                throw new ErrorResponse(
+                    "You are not authorized to view this resource",
+                    401
+                );
+            }
+        } catch (error) {
+            next(error);
+        }
+    };
 };
--- routes/proxyauth.js
+++ routes/proxyauth.js
@@ -1,35 +1,35 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
-import { session_middleware, checkSession } from '../middlewares/sessions.js';
-import { checkPermissionLevel } from '../middlewares/permissions.js';
+import { session_middleware, checkSession } from "../middlewares/sessions.js";
+import { checkPermissionLevel } from "../middlewares/permissions.js";
 
 // Import controllers
 import {
-	registerRequest,
-	registerResponse,
-	signInRequest,
-	signInResponse,
-} from '../controllers/webauthn.js';
-import { proxyIdUse, proxyIdCheck } from '../middlewares/proxyauth.js';
-import { proxyAuthCreate, proxyAuthCheck } from '../controllers/proxyauth.js';
+    registerRequest,
+    registerResponse,
+    signInRequest,
+    signInResponse,
+} from "../controllers/webauthn.js";
+import { proxyIdUse, proxyIdCheck } from "../middlewares/proxyauth.js";
+import { proxyAuthCreate, proxyAuthCheck } from "../controllers/proxyauth.js";
 
 // ------- Exposed Endpoints -------------
-router.post('/create', proxyAuthCreate);
+router.post("/create", proxyAuthCreate);
+router.post("/signInRequest", proxyIdCheck, signInRequest);
+router.post("/signInResponse", proxyIdCheck, signInResponse, proxyIdUse);
 
 // ---------------- ROUTES ----------------
 
-// router.use(checkPermissionLevel('GUEST'));
+router.use(checkPermissionLevel("GUEST"));
 router.use(session_middleware);
 
-router.post('/registerRequest', proxyIdCheck, registerRequest);
+router.post("/registerRequest", checkSession, proxyIdCheck, registerRequest);
 router.post(
-	'/registerResponse',
-
-	proxyIdCheck,
-	registerResponse,
-	proxyIdUse
+    "/registerResponse",
+    checkSession,
+    proxyIdCheck,
+    registerResponse,
+    proxyIdUse
 );
-router.post('/signInRequest', proxyIdCheck, signInRequest);
-router.post('/signInResponse', proxyIdCheck, signInResponse, proxyIdUse);
 
 export default router;
